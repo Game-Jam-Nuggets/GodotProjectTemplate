2851d110b4304b5de0c91cc8e11646e3ea519e6e
Kylen Xiao <ZeldaSpider599@gmail.com>
2026-02-24 19:33:14 -0700

    Added UI scenes.

--- scenes/autoload/scene_manager.gd
+++ scenes/autoload/scene_manager.gd
@@ -4,18 +4,23 @@ extends Node
 var current_scene: Node = null
 
 ## --- export vars ---
-@export var main_menu_scene: PackedScene
+@export var scenes: Dictionary[String, PackedScene]
 
 ## --- default methods ---
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	load_main_menu_scene()
+	change_room("main_menu")
 	
 ## --- public methods ---
 
 ## Changes the room
-func change_room(scene: PackedScene) -> void:
+func change_room(scene_name: String) -> void:
+	if !scenes.has(scene_name):
+		return
+		
+	var scene = scenes[scene_name]
+	
 	# Remove current room
 	if current_scene and is_instance_valid(current_scene):
 		current_scene.queue_free()
@@ -23,7 +28,3 @@ func change_room(scene: PackedScene) -> void:
 	# Load new room
 	current_scene = scene.instantiate()
 	add_child(current_scene)
-
-## Loads the main menu scene
-func load_main_menu_scene():
-	change_room(main_menu_scene)
